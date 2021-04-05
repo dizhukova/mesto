423eb16af6207ca85b35054afe702eb1f743b92e
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-04-05 14:28:50 +0300

    fix: fixing mistakes after first review

---
+++ scripts/initial-сards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Италия',
+      link: 'https://images.unsplash.com/photo-1536323760109-ca8c07450053?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2602&q=80'
+    },
+    {
+      name: 'Франция',
+      link: 'https://images.unsplash.com/photo-1587889834738-eb80f47da2e3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=882&q=80'
+    },
+    {
+      name: 'Грузия',
+      link: 'https://images.unsplash.com/photo-1568558715456-0983a3b5cca9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=975&q=80'
+    },
+    {
+      name: 'Турция',
+      link: 'https://images.unsplash.com/photo-1526048598645-62b31f82b8f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+      name: 'Куба',
+      link: 'https://images.unsplash.com/photo-1570299437522-f66ff98d52e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+      name: 'Шри-Ланка',
+      link: 'https://images.unsplash.com/photo-1571406761758-9a3eed5338ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+    }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,54 @@
 // форма редактирования профиля
-let popupEditForm = document.querySelector('.popup_edit_profile');
-let openEditForm = document.querySelector('.profile__edit-button');
-let closeEditForm = document.querySelector('#edit_close-button');
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let form = document.querySelector('#form_edit-profile');
-let editName = document.querySelector('.popup__form-input_type_name');
-let editProfession = document.querySelector('.popup__form-input_type_profession');
+const popupEditForm = document.querySelector('.popup_edit_profile');
+const openEditPopupBtn = document.querySelector('.profile__edit-button');
+const closeEditPopupBtn = document.querySelector('#edit_close-button');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+const editForm = document.querySelector('#form_edit-profile');
+const editName = document.querySelector('.popup__form-input_type_name');
+const editProfession = document.querySelector('.popup__form-input_type_profession');
 
-openEditForm.addEventListener('click', function openEditPopup() {
-    popupEditForm.classList.add('popup_opened');
+// открытие попапа
+function openPopup(popup) {
+    popup.classList.add('popup_opened'); 
+}
+
+// закрытие попапа
+function closePopup(popup) {
+    popup.classList.remove('popup_opened'); 
+}
+
+openEditPopupBtn.addEventListener('click', function openEditPopup() {
+    openPopup(popupEditForm);
     editName.value = profileName.textContent;
     editProfession.value = profileProfession.textContent;
 });
 
-function closeEditPopup() {
-    popupEditForm.classList.remove('popup_opened');
-}
+closeEditPopupBtn.addEventListener('click', function closeEditPopup(){
+    closePopup(popupEditForm);
+});
 
-closeEditForm.addEventListener('click', closeEditPopup);
-
-form.addEventListener('submit', function handleProfileSubmit(evt) {
+function handleProfileSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = editName.value;
     profileProfession.textContent = editProfession.value;
-    closeEditPopup();
-});
+    closePopup(popupEditForm);
+}
+
+editForm.addEventListener('submit', handleProfileSubmit);
 
 
 // загрузка карточек на страницу
-const initialCards = [
-    {
-      name: 'Италия',
-      link: 'https://images.unsplash.com/photo-1536323760109-ca8c07450053?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2602&q=80'
-    },
-    {
-      name: 'Франция',
-      link: 'https://images.unsplash.com/photo-1587889834738-eb80f47da2e3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=882&q=80'
-    },
-    {
-      name: 'Грузия',
-      link: 'https://images.unsplash.com/photo-1568558715456-0983a3b5cca9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=975&q=80'
-    },
-    {
-      name: 'Турция',
-      link: 'https://images.unsplash.com/photo-1526048598645-62b31f82b8f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80'
-    },
-    {
-      name: 'Куба',
-      link: 'https://images.unsplash.com/photo-1570299437522-f66ff98d52e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
-    },
-    {
-      name: 'Шри-Ланка',
-      link: 'https://images.unsplash.com/photo-1571406761758-9a3eed5338ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
-    }
-];
-
 const elements = document.querySelector('.elements')
-const CardTemplate = document.querySelector('.template');
+const cardTemplate = document.querySelector('.template');
 
-const ImagePopup = document.querySelector('.popup_open_image');
-const closeImagePopup = document.querySelector('#image_close-button');
+const imagePopup = document.querySelector('.popup_open_image');
+const closeImagePopupBtn = document.querySelector('#image_close-button');
 const pictureImagePopup = document.querySelector('.popup__image');
 const captionImagePopup = document.querySelector('.popup__caption');
 
 function createCard(element) {
-    const card = CardTemplate.content.cloneNode(true);
+    const card = cardTemplate.content.cloneNode(true);
     const cardTitle = card.querySelector('.element__name');
     const cardImage = card.querySelector('.element__image');
 
@@ -87,7 +70,7 @@ function createCard(element) {
 
     // открытие карточки
     cardImage.addEventListener('click', function openImagePopup() {
-        ImagePopup.classList.add('popup_opened');
+        openPopup(imagePopup);
         pictureImagePopup.src = element.link;
         pictureImagePopup.alt = element.name;
         captionImagePopup.textContent = element.name;
@@ -97,34 +80,34 @@ function createCard(element) {
 };
 
 function renderCards() {
-    const photoCard = initialCards.map(createCard);
-    elements.append(...photoCard);
+    const photoCards = initialCards.map(createCard);
+    elements.append(...photoCards);
 }
 
 renderCards();
 
 // закрытие карточки
-closeImagePopup.addEventListener('click', function() {
-    ImagePopup.classList.remove('popup_opened');
+closeImagePopupBtn.addEventListener('click', function() {
+    closePopup(imagePopup);
 });
 
 // форма добавления новой карточки
 const popupAddForm = document.querySelector('.popup_add_card');
-const openAddForm = document.querySelector('.profile__add-button');
-const closeAddForm = document.querySelector('#add_close-button');
+const openAddFormBtn = document.querySelector('.profile__add-button');
+const closeAddFormBtn = document.querySelector('#add_close-button');
 const addForm = document.querySelector('#form_add-card');
 
-openAddForm.addEventListener('click', function openAddPopup() {
-    popupAddForm.classList.add('popup_opened');
+openAddFormBtn.addEventListener('click', function openAddPopup() {
+    openPopup(popupAddForm);
 });
 
 function closeAddPopup() {
-    popupAddForm.classList.remove('popup_opened');
+    closePopup(popupAddForm);
 }
 
-closeAddForm.addEventListener('click', closeAddPopup);
+closeAddFormBtn.addEventListener('click', closeAddPopup);
 
-addForm.addEventListener('submit', function addNewCard(evt) {
+function addNewCard(evt) {
     evt.preventDefault();
     const inputName = addForm.querySelector('.popup__form-input_card_name');
     const inputLink = addForm.querySelector('.popup__form-input_card_link');
@@ -135,5 +118,7 @@ addForm.addEventListener('submit', function addNewCard(evt) {
     elements.prepend(newCard);
 
     addForm.reset();
-    closeAddPopup();
-});
+    closePopup(popupAddForm);
+}
+
+addForm.addEventListener('submit',addNewCard);
