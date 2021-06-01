95e6d7de81934aab894f3606b9a699a412a41423
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-06-01 16:31:54 +0300

    fix: fix typo ikes in unlike api

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -77,7 +77,7 @@ export default class Api {
     }
 
     unlikeCard(cardId) {
-        return fetch(`${this._baseUrl}/cards/ikes/${cardId}`, {
+        return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
             method: 'DELETE',
             headers: this._headers,
         })
